727f95d1bee0c91b3706e877cb07759105dc0388
mahdi jalali <jalali.m.mahdi@gmail.com>
2020-05-24 14:56:30 +0430

    adding some samples and test

--- DataGather.py
+++ DataGather.py
@@ -1,9 +1,11 @@
 import binance
 import setting
 
-#print(binance.binance_findServerTime())
-# data=binance.binance_orderBook().get('bids')
-# for x in data:
-#     print(x)
+print(binance.binance_findServerTime())
 
+#print(binance.binance_exchangeInfo())
+
+#it shows the bid and ask list:
 binance.binance_orderBook_printData()
+
+print(binance.binance_tradesList_recent())
--- binance.py
+++ binance.py
@@ -9,7 +9,7 @@ API_EndPoint="https://api.binance.com"
 
 def funcGetDataFromBinance(myQuery):
     link = f"{API_EndPoint}{myQuery}"
-    print(link)
+    print(f"command is: {link}")
     hdr = { 'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; WOW64)' }
     response = requests.get(link, verify=True,headers=hdr)
     myfile = response.text
@@ -25,12 +25,19 @@ def binance_findServerTime():
     Server_Time = datetime.datetime.fromtimestamp(ServerTime_TimeStamp).ctime()
     return Server_Time
 
+# پرینت لیستی از نمادها و وضعیت هر یک
 def binance_exchangeInfo():
     query_info="/api/v3/exchangeInfo"
     data=funcGetDataFromBinance(query_info)
     return data
 
+
 #this prints list of bids and asks in Binance
+# Limit 	            Weight
+# 5, 10, 20, 50, 100 	1
+# 500                	5
+# 1000 	                10
+# 5000 	                50
 def binance_orderBook(symbol=mysymbol,limit=5):
     query_orderbook=f"/api/v3/depth?symbol={symbol}&limit={limit}"
     data=funcGetDataFromBinance(query_orderbook)
@@ -50,5 +57,11 @@ def binance_orderBook_printData(symbol=mysymbol,limit=10):
     for x in asks:
         print(x)
 
-    return 1
+#To show list of bids and asks (just for help me)
+def binance_tradesList_recent(symbol=mysymbol,limit=10):
+    query_orderbook=f"/api/v3/trades?symbol={symbol}&limit={limit}"
+    data=funcGetDataFromBinance(query_orderbook)
 
+    return data
+
+        
